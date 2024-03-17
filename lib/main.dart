607f552f3511fe02a378eb6b864106dbf4b9be23
knndy-Mr <kenniemcf@gmail.com>
2024-03-16 20:04:54 -0500

    study scyn calendar

---
+++ lib/main.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+import 'event.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  Map<DateTime, List<Event>> events = {};
+  TextEditingController _eventController = TextEditingController();
+  late final ValueNotifier<List<Event>> _selectedEvents;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDay = _focusedDay;
+    _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _eventController.dispose();
+    _selectedEvents.dispose();
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      _selectedDay = selectedDay;
+      _focusedDay = focusedDay;
+      _selectedEvents.value = _getEventsForDay(selectedDay);
+    });
+  }
+
+  List<Event> _getEventsForDay(DateTime day) {
+    return events[day] ?? [];
+  }
+
+  void _deleteAssignment(Event event) {
+    setState(() {
+      events[_selectedDay!]!.remove(event);
+      _selectedEvents.value = _getEventsForDay(_selectedDay!);
+    });
+  }
+
+  void _showDeleteConfirmationDialog(Event event) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Delete Assignment"),
+        content: Text("Are you sure you want to delete this assignment?"),
+        actions: [
+          TextButton(
+            onPressed: () {
+              _deleteAssignment(event);
+              Navigator.pop(context);
+            },
+            child: Text(
+              "Delete",
+              style: TextStyle(color: Colors.red),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text("Cancel"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Welcome to StudySync Calendar!"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                scrollable: true,
+                title: Text("Assignment Name"),
+                content: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: TextField(
+                    controller: _eventController,
+                  ),
+                ),
+                actions: [
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_eventController.text.isNotEmpty) {
+                        setState(() {
+                          String assignmentName = _eventController.text;
+                          events.update(
+                            _selectedDay!,
+                            (value) => [...value, Event(assignmentName)],
+                            ifAbsent: () => [Event(assignmentName)],
+                          );
+                          _selectedEvents.value = _getEventsForDay(_selectedDay!);
+                          _eventController.clear();
+                        });
+                        Navigator.of(context).pop();
+                      } else {
+                        // Handle case when text field is empty
+                        // For example, show a snackbar or an error message
+                      }
+                    },
+                    child: Text("Submit"),
+                  )
+                ],
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: DateTime.utc(2010, 10, 16),
+            lastDay: DateTime.utc(2030, 3, 14),
+            focusedDay: _focusedDay,
+            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+            calendarFormat: _calendarFormat,
+            onDaySelected: _onDaySelected,
+            eventLoader: _getEventsForDay,
+            calendarStyle: CalendarStyle(
+              outsideDaysVisible: false,
+            ),
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
+            },
+            onPageChanged: (focusedDay) {
+              setState(() {
+                _focusedDay = focusedDay;
+              });
+            },
+          ),
+          SizedBox(height: 8.0),
+          Expanded(
+            child: ValueListenableBuilder(
+              valueListenable: _selectedEvents,
+              builder: (context, value, _) {
+                return ListView.builder(
+                  itemCount: value.length,
+                  itemBuilder: (context, index) {
+                    final assignment = value[index];
+                    return GestureDetector(
+                      onDoubleTap: () => _showDeleteConfirmationDialog(assignment),
+                      child: Container(
+                        margin: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                        decoration: BoxDecoration(
+                          border: Border.all(),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: ListTile(
+                          onTap: () => print(""),
+                          title: Text(assignment.title),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
